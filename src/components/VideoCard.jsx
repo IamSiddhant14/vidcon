8890d7a8a9c54fa47315f1bfa8c718ebf6ded26b
Siddhant Sharma <siddhantsharma669@gmail.com>
2022-12-19 01:38:15 +0530

    removed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia } from '@mui/material'
 
 import { CheckCircle } from '@mui/icons-material';
-
-import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
+// demoThumbnailUrl,
+import {  demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
   return (
